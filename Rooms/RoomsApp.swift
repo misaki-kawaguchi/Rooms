0814c36a6366f7f8ca7c77aa167d74dce7e1c955
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-05-14 08:55:44 +0900

    first commit

---
+++ Rooms/RoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsApp.swift
+//  Rooms
+//
+//  Created by 川口美咲 on 2024/05/14.
+//
+
+import SwiftUI
+
+@main
+struct RoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
