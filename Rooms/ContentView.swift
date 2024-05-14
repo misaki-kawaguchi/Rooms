a460a442f033da986db1b01ff282d349239a50cb
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2024-05-14 09:49:56 +0900

    UI作成 (#3)

--- Rooms/ContentView.swift
+++ Rooms/ContentView.swift
@@ -7,13 +7,45 @@
 
 import SwiftUI
 
+struct ColorSelectorView: View {
+    @Binding var selection: Color
+    let colors: [Color] = [.red, .orange, .purple, .blue, .yellow, .indigo]
+    
+    var body: some View {
+        HStack {
+            ForEach(colors, id: \.self) { color in
+                Circle()
+                    .fill(color)
+                    .overlay {
+                        selection == color ? Circle().stroke(Color.black, lineWidth: 2) : nil
+                    }
+                    .onTapGesture {
+                        selection = color
+                    }
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @State private var name: String = ""
+    @State private var color: Color = .red
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Name", text: $name)
+                .textFieldStyle(.roundedBorder)
+            
+            ColorSelectorView(selection: $color)
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Save")
+                    .frame(maxWidth: .infinity)
+            })
+            .buttonStyle(.borderedProminent)
+            .padding(.top, 20)
+            
+            Spacer()
         }
         .padding()
     }
