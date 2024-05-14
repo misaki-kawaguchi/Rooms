90985473ec83ff8445dccb3455d4ef09df2f08e7
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-05-14 09:07:22 +0900

    Roomモデルを作成

---
+++ Rooms/Models/Room.swift
@@ -0,0 +1,21 @@
+//
+//  Room.swift
+//  Rooms
+//
+//  Created by 川口美咲 on 2024/05/14.
+//
+
+import Foundation
+import SwiftData
+import UIKit
+
+@Model
+class Room {
+    var name: String
+    var color: UIColor
+    
+    init(name: String, color: UIColor) {
+        self.name = name
+        self.color = color
+    }
+}
